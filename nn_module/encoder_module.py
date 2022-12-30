9cc72cc82716c67ce03f0fc85cf68d1dfe7090f4
andrey18b <100760998+andrey18b@users.noreply.github.com>
2022-12-30 21:59:46 +0100

    Adding All Files

---
+++ nn_module/encoder_module.py
@@ -0,0 +1,222 @@
+import torch
+from torch import nn
+from .attention_module import PreNorm, StandardAttention, FeedForward, LinearAttention, ReLUFeedForward
+
+class TransformerCatNoCls(nn.Module):
+    def __init__(self,
+                 dim,
+                 depth,
+                 heads,
+                 dim_head,
+                 mlp_dim,
+                 attn_type,  # ['standard', 'galerkin', 'fourier']
+                 use_ln=False,
+                 scale=16,     # can be list, or an int
+                 dropout=0.,
+                 relative_emb_dim=2,
+                 min_freq=1/64,
+                 attention_init='orthogonal',
+                 init_gain=None,
+                 use_relu=False,
+                 cat_pos=False,
+                 ):
+        super().__init__()
+        assert attn_type in ['standard', 'galerkin', 'fourier']
+
+        if isinstance(scale, int):
+            scale = [scale] * depth
+        assert len(scale) == depth
+
+        self.layers = nn.ModuleList([])
+        self.attn_type = attn_type
+        self.use_ln = use_ln
+
+        if attn_type == 'standard':
+            for _ in range(depth):
+                self.layers.append(
+                    nn.ModuleList([
+                    PreNorm(dim, StandardAttention(dim, heads=heads, dim_head=dim_head, dropout=dropout)),
+                    PreNorm(dim,  FeedForward(dim, mlp_dim, dropout=dropout)
+                                  if not use_relu else ReLUFeedForward(dim, mlp_dim, dropout=dropout))]),
+                )
+        else:
+            for d in range(depth):
+                if scale[d] != -1 or cat_pos:
+                    attn_module = LinearAttention(dim, attn_type,
+                                                   heads=heads, dim_head=dim_head, dropout=dropout,
+                                                   relative_emb=True, scale=scale[d],
+                                                   relative_emb_dim=relative_emb_dim,
+                                                   min_freq=min_freq,
+                                                   init_method=attention_init,
+                                                   init_gain=init_gain
+                                                   )
+                else:
+                    attn_module = LinearAttention(dim, attn_type,
+                                                  heads=heads, dim_head=dim_head, dropout=dropout,
+                                                  cat_pos=True,
+                                                  pos_dim=relative_emb_dim,
+                                                  relative_emb=False,
+                                                  init_method=attention_init,
+                                                  init_gain=init_gain
+                                                  )
+                if not use_ln:
+                    self.layers.append(
+                        nn.ModuleList([
+                                        attn_module,
+                                        FeedForward(dim, mlp_dim, dropout=dropout)
+                                        if not use_relu else ReLUFeedForward(dim, mlp_dim, dropout=dropout)
+                        ]),
+                        )
+                else:
+                    self.layers.append(
+                        nn.ModuleList([
+                            nn.LayerNorm(dim),
+                            attn_module,
+                            nn.LayerNorm(dim),
+                            FeedForward(dim, mlp_dim, dropout=dropout)
+                            if not use_relu else ReLUFeedForward(dim, mlp_dim, dropout=dropout),
+                        ]),
+                    )
+
+    def forward(self, x, pos_embedding):
+        # x in [b n c], pos_embedding in [b n 2]
+        b, n, c = x.shape
+
+        for layer_no, attn_layer in enumerate(self.layers):
+            if not self.use_ln:
+                [attn, ffn] = attn_layer
+
+                x = attn(x, pos_embedding) + x
+                x = ffn(x) + x
+            else:
+                [ln1, attn, ln2, ffn] = attn_layer
+                x = ln1(x)
+                x = attn(x, pos_embedding) + x
+                x = ln2(x)
+                x = ffn(x) + x
+        return x
+
+class SpatialEncoder2D(nn.Module):
+    def __init__(self,
+                 input_channels,           # how many channels
+                 in_emb_dim,               # embedding dim of token                 (how about 512)
+                 out_seq_emb_dim,          # embedding dim of encoded sequence      (how about 256)
+                 heads,
+                 depth,                    # depth of transformer / how many layers of attention    (4)
+                 res,
+                 use_ln=True,
+                 emb_dropout=0.05,           # dropout of embedding
+                 ):
+        super().__init__()
+
+        self.to_embedding = nn.Sequential(
+            nn.Linear(input_channels, in_emb_dim, bias=False),
+        )
+
+        self.dropout = nn.Dropout(emb_dropout)
+
+        self.s_transformer = TransformerCatNoCls(in_emb_dim, depth, heads, in_emb_dim, in_emb_dim,
+                                                 'galerkin',
+                                                 use_relu=False,
+                                                 use_ln=use_ln,
+                                                 scale=[res, res//4] + [1]*(depth-2),
+                                                 relative_emb_dim=2,
+                                                 min_freq=1 / res,
+                                                 dropout=0.03,
+                                                 attention_init='orthogonal')
+
+        self.to_out = nn.Sequential(
+            nn.Linear(in_emb_dim, out_seq_emb_dim, bias=False))
+
+    def forward(self,
+                x,  # [b, n, c]
+                input_pos,  # [b, n, 2]
+                ):
+
+        x = self.to_embedding(x)
+        x = self.dropout(x)
+
+        x = self.s_transformer.forward(x, input_pos)
+        x = self.to_out(x)
+
+        return x
+    
+class SpatialTemporalEncoder2D(nn.Module):
+    def __init__(self,
+                 input_channels,           # how many channels
+                 in_emb_dim,               # embedding dim of token                 (how about 512)
+                 out_seq_emb_dim,          # embedding dim of encoded sequence      (how about 256)
+                 heads,
+                 depth,                    # depth of transformer / how many layers of attention    (4)
+                 ):
+        super().__init__()
+
+        self.to_embedding = nn.Sequential(
+            # Rearrange('b c n -> b n c'),
+            nn.Linear(input_channels, in_emb_dim, bias=False),
+        )
+
+        if depth > 4:
+            self.s_transformer = TransformerCatNoCls(in_emb_dim, depth, heads, in_emb_dim, in_emb_dim,
+                                                     'galerkin', True, scale=[32, 16, 8, 8] +
+                                                                             [1] * (depth - 4),
+                                                     attention_init='orthogonal')
+        else:
+            self.s_transformer = TransformerCatNoCls(in_emb_dim, depth, heads, in_emb_dim, in_emb_dim,
+                                                     'galerkin', True, scale=[32] + [16]*(depth-2) + [1],
+                                                     attention_init='orthogonal')
+
+        self.project_to_latent = nn.Sequential(
+            nn.Linear(in_emb_dim, out_seq_emb_dim, bias=False))
+
+    def forward(self,
+                x,  # [b, t(*c)+2, n]
+                input_pos,  # [b, n, 2]
+                ):
+
+        x = self.to_embedding(x)
+        x = self.s_transformer.forward(x, input_pos)
+        x = self.project_to_latent(x)
+
+        return x
+    
+class Encoder1D(nn.Module):
+    def __init__(self,
+                 input_channels,           # how many channels
+                 in_emb_dim,               # embedding dim of token                 (how about 512)
+                 out_seq_emb_dim,          # embedding dim of encoded sequence      (how about 256)
+                 depth,                    # depth of transformer / how many layers of attention    (4)
+                 emb_dropout=0.05,           # dropout of embedding
+                 res=2048,
+                 ):
+        super().__init__()
+
+        # self.dropout = nn.Dropout(emb_dropout)
+
+        self.to_embedding = nn.Sequential(
+            nn.Linear(input_channels, in_emb_dim-1, bias=False),
+        )
+
+        self.transformer = TransformerCatNoCls(in_emb_dim, depth, 1, in_emb_dim, in_emb_dim, 'fourier',
+                                               scale=[8.] + [4.]*2 + [1.]*(depth-3),
+                                               relative_emb_dim=1,
+                                               min_freq=1/res,
+                                               attention_init='orthogonal')
+
+        self.project_to_latent = nn.Sequential(
+            nn.Linear(in_emb_dim, out_seq_emb_dim, bias=False))
+
+    def forward(self,
+                x,  # [b, n, c]
+                input_pos,  # [b, n, 1]
+                ):
+        x = self.to_embedding(x)
+        # x = self.dropout(x)
+        x = torch.cat((x, input_pos), dim=-1)
+        x = self.transformer.forward(x, input_pos)
+        x = self.project_to_latent(x)
+
+        return x
+
+
+
